27c3b2ea5df73a2e28be440c81ec1402433973de
hidenari-pixel <hide8800kasu@gmail.com>
2022-09-01 18:05:24 +0900

    LoadingSpinner改良

--- frontend/src/components/LoadingSpinner.tsx
+++ frontend/src/components/LoadingSpinner.tsx
@@ -1,7 +1,23 @@
 import Box from '@mui/material/Box';
-import CircularProgress from '@mui/material/CircularProgress';
+import CircularProgress, { CircularProgressProps } from '@mui/material/CircularProgress';
 
-export const LoadingSpinner = () => {
+type LoadingSpinnerProps = CircularProgressProps & {
+  size?: keyof typeof sizes;
+};
+
+const sizes = {
+  sm: 20,
+  md: 40,
+  lg: 60,
+  xl: 100,
+};
+
+const variants = {
+  primary: 'determinate',
+  secondary: 'indeterminate',
+};
+
+export const LoadingSpinner = ({ size = 'md', ...rest }: LoadingSpinnerProps) => {
   return (
     <Box
       sx={{
@@ -12,7 +28,7 @@ export const LoadingSpinner = () => {
         alignItems: 'center',
       }}
     >
-      <CircularProgress />
+      <CircularProgress size={sizes[size]} {...rest} />
     </Box>
   );
 };
