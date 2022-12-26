e58b4c18e42b3fb670acb3b29a159007061431d9
hidenari-pixel <hide8800kasu@gmail.com>
2022-12-26 19:58:52 +0900

    時間表示を修正 → safariでも閲覧可

--- frontend/src/pages/reviews/[review_id]/[reviewed_id].tsx
+++ frontend/src/pages/reviews/[review_id]/[reviewed_id].tsx
@@ -7,7 +7,6 @@ import SubHeader from '@/features/reviews/components/SubHeader';
 import UploadFileArea from '@/features/reviews/components/UploadFileArea';
 import { pdfReviewState } from '@/shared/stores';
 import { Box, Chip, Divider, Stack, Typography } from '@mui/material';
-import { format } from 'date-fns';
 import { NextPage } from 'next';
 import { useRouter } from 'next/router';
 import { useState } from 'react';
@@ -114,7 +113,9 @@ const ReviewedFiles: NextPage = () => {
               <Box>
                 <Typography sx={{ fontSize: '14px', mx: 1 }}>{reviewedFile.file_name}</Typography>
                 <Typography sx={{ fontSize: '10px', mx: 1 }}>
-                  {format(new Date(reviewedFile.created_at), 'yyyy-MM-dd hh:mm')}
+                  {reviewedFile.created_at
+                    .replaceAll('-', '/')
+                    .slice(0, reviewedFile.created_at.indexOf('+') - 1)}
                 </Typography>
               </Box>
               <Chip size="small" label={`${reviewedFile.reviewer_count}件のレビュー`} />
